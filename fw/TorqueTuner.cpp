308d37b61f8e34a91a4e158af645ec74785558bc
Christian Frisson <christian.frisson@gmail.com>
2021-09-01 00:22:19 -0400

    Update TorqueTuner<->moteus bindings

--- fw/TorqueTuner.cpp
+++ fw/TorqueTuner.cpp
@@ -37,6 +37,8 @@ float velocity = 0;
 int16_t torque = 0;
 int32_t err = 0;
 
+BldcServo::CommandData prev_command;
+
 uint16_t calcsum(uint8_t buf[], int length) {
   uint32_t val = 0;
   for (int k = 0; k < length; k++) {
@@ -48,6 +50,13 @@ uint16_t calcsum(uint8_t buf[], int length) {
 
 void receiveI2C(int how_many) {
   uint8_t k = 0;
+  BldcServo::CommandData stop_command;
+  stop_command.mode = BldcServo::kStopped;
+  if(bldcServo && bldcServo->status().fault == moteus::errc::kStartOutsideLimit ){
+    stop_command.rezero_position = 0.0f;
+    bldcServo->Command(stop_command);
+  }
+  float kNaN = std::numeric_limits<float>::quiet_NaN();
   while (available()) {
     rx_data[k] = read();
     k++;
@@ -61,7 +70,7 @@ void receiveI2C(int how_many) {
     memcpy(&torque, rx_data, 2); // int16
     memcpy(&velocity, rx_data + 2, 4); // float
     memcpy(&mode_rec, rx_data + 6, 1); // char
-    
+
     BldcServo::CommandData command;
     // We default to no timeout for debug commands.
     command.timeout_s = std::numeric_limits<float>::quiet_NaN();
@@ -70,21 +79,24 @@ void receiveI2C(int how_many) {
     command.mode = BldcServo::kPosition; // kZeroVelocity
     
     if (mode_rec == 't'){
+      command.position = kNaN; // kNaN means start at the current position.
+      command.velocity = 0.0f;
       command.kd_scale = 0.0f;
       command.kp_scale = 0.0f;
-      command.feedforward_Nm = static_cast<float> (torque);
-    }
-    if (mode_rec == 'x'){
-      // command.position = ...;
-      // TODO
+      command.feedforward_Nm = static_cast<float> (torque) / 208.0f; // 208.0f is an approximate value of Mechaduino uMAX
+      if(bldcServo && prev_command.feedforward_Nm != command.feedforward_Nm){
+        bldcServo->Command(command);
+      }
     }
     if (mode_rec == 'v') {
       command.velocity = velocity;
-      // TODO
-    };
-    // command.max_torque_Nm = ...;
-
-    if(bldcServo) bldcServo->Command(command);
+      command.position = kNaN; // kNaN means start at the current position.
+      // TODO check max_position_slip
+      if(bldcServo && prev_command.velocity != command.velocity){
+        bldcServo->Command(command);
+      }
+    }
+    prev_command = command;
   }
 
 }
@@ -93,14 +105,14 @@ void sendI2C() {
   // Angle
   tmp = 0;//y_1;
   if(bldcServo) tmp = bldcServo->status().unwrapped_position;
-  angle_rounded = static_cast<int16_t> (tmp /** 10 + 0.5f*/);
+  angle_rounded = static_cast<int16_t> (tmp * 3600 + 0.5f);
   // angle_rounded = static_cast<int16_t> (read_angle() * 10 + 0.5);
   memcpy(tx_data, &angle_rounded, 2);
 
   // wrapped angle_rounded
   tmp = 0;//yw - PA;
   if(bldcServo) tmp = bldcServo->status().position;
-  angle_rounded = static_cast<int16_t>(tmp /** 10 + 0.5f*/);
+  angle_rounded = static_cast<int16_t>(tmp * 3600 + 0.5f);
   memcpy(tx_data + 2, &angle_rounded, 2); // uint
 
   // Velocity
